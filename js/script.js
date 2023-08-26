1525478a55090776a0d538d7cc7876cfefdcd9f6
sirpauti <131233097+sirpauti@users.noreply.github.com>
2023-08-26 12:29:25 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,46 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+// Función que recibe por parámetro un array y muestra sus elementos en pantalla
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  
+// Filtrar y ordenar elementos de tipo string
+  const filteredStrings = array.filter((element) => typeof element === "string").sort();
+
+// Mostrar los elementos filtrados y ordenados en pantalla
+  filteredStrings.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  // Escribe tu solución aquí
+  showList(strangeArray);
+});
